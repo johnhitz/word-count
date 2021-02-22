a129a0dc4dfef0f9c3e4249c2f660ac801457a79
John Hitz <johnh@whodapet.com>
2021-02-22 11:39:05 -0600

    Refactored using a regex and trim: true in my String.split method.

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -8,36 +8,16 @@ defmodule WordCount do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.split()
+    |> String.split(~r/[^[:alnum:]-]/u, trim: true)
     |> Enum.reduce(%{}, &get_word_count/2)
   end
 
   defp get_word_count(word, acc) do
-    has_underscore = Regex.match?(~r/[a-zA-Z]+_[a-zA-Z]+/, word)
     cond do
       Map.has_key?(acc, word) ->
         value = Map.get(acc, word)
         %{acc | word => value + 1}
-      has_underscore ->
-        remove_underscore(word, acc)
-      true -> is_word?(word, acc)
+      true -> Map.put(acc, word, 1)
     end
-  end
-
-  defp is_word?(word, acc) do
-    case word do
-      word when word not in [":"] ->
-        Map.put(acc, remove_punctuation(word), 1)
-      _ -> acc
-    end
-  end
-
-  defp remove_punctuation(word) do
-    Regex.replace(~r/[!&@$%^,]/, word, "")
-  end
-
-  defp remove_underscore(word, acc) do
-    words = String.split(word, "_")
-    Enum.reduce(words, acc, fn x, acc -> get_word_count(x, acc) end)
   end
 end
