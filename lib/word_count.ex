5bde9f950a3408a6676fd1a2a77b04edb22c3f4a
John Hitz <johnh@whodapet.com>
2021-02-23 13:47:56 -0600

    Used Map.update/4 with a caputureing function as the 4th argument

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -8,16 +8,8 @@ defmodule WordCount do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.split(~r/[^[:alnum:]-]/u, trim: true)
-    |> Enum.reduce(%{}, &get_word_count/2)
-  end
-
-  defp get_word_count(word, acc) do
-    cond do
-      Map.has_key?(acc, word) ->
-        value = Map.get(acc, word)
-        %{acc | word => value + 1}
-      true -> Map.put(acc, word, 1)
-    end
+    # |> String.split(~r/[^[:alnum:]-]/u, trim: true)
+    |> String.split()
+    |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
